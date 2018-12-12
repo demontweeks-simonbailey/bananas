b0b9860b2ecbe5e267edee7fee50a11e26d1b9d0
Simon Bailey <simon.bailey@parcelpet.com>
2018-12-12 09:39:09 +0000

    Changed to array

--- bananas.php
+++ bananas.php
@@ -1,3 +1,11 @@
 <?php
-$bananas = "Bananas not only taste good, but the word sounds funny (Banana Dependency)<br />";
+
+  $bananasArray = [];
+
+  array_push($bananasArray,"Bananas not only taste good, but the word sounds funny (Banana Dependency)");
+  array_push($bananasArray,"High in energy");
+
+  for($i = 0; $i < count($bananasArray); $i++){
+    $bananas .= $bananasArray[$i] . $br;
+  }
 ?>
