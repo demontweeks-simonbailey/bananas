df06204c9c5d394f904c9e3b449e2e73326e88ec
Simon Bailey <demontweeks-simonbailey@users.noreply.github.com>
2018-12-12 10:39:37 +0000

    Last update from ipad

--- bananas.php
+++ bananas.php
@@ -4,6 +4,7 @@
 
   array_push($bananasArray,"Bananas not only taste good, but the word sounds funny (Banana Dependency)");
   array_push($bananasArray,"High in energy");
+  array_push($bananasArray,"Last one...I swear");
 
   for($i = 0; $i < count($bananasArray); $i++){
     $bananas .= $bananasArray[$i] . $br;
